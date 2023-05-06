f1e728999ee759bb4e720ce96095a9a272d40eca
Castel <keviniicastel@gmail.com>
2023-05-06 12:29:02 +0000

    demo non termine

--- quadchecker.go
+++ quadchecker.go
@@ -1,7 +1,41 @@
 //go 1.10.4
 
 package main
-import "fmt"
+
+import ("fmt"
+       "os")
+
+
 func main() {
-  fmt.Printf("Hello, Dcoder!")
+  args := os.Args
+  if len(args) >= 2{
+    m := GetArgs(args[1:])
+    fmt.Println(m)
+  } else {
+    fmt.Println("Not a quad function")
+  }
+}
+
+
+func GetArgs(a[]string) map[string]string{
+  mapQuadInformation := map[string]string{}
+  for _, i := range a{
+    fmt.Println(i)
+    
+  }
+  return mapQuadInformation
+}
+
+func IsInt(s string) bool{
+  var b bool
+  
+  return b
+}
+
+
+
+func quadchecker(){
+  /*
+    Exercice function
+  */
 }
