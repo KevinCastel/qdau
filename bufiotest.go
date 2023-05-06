7a3e637e98f4d7c34960996f2da29856dc1750dc
KevinCastel <keviniicastel@gmail.com>
2023-05-06 20:26:18 +0200

    Ce fichier m'a permis d'expérimenter avec le go pour la sortie console

---
+++ bufiotest.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("go", "run", "./quadD.go")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Erreur lors de l'exécution:", err)
+		fmt.Println("Sortie d'erreur:")
+		fmt.Println(string(output))
+		return
+	}
+
+	/*
+		La partie chatGPT en haut
+		Ma partie en bas
+	*/
+
+	lineLength := 0
+	countLine := 0
+	isLineLengthFound := false
+
+	for _, char := range output {
+		if !isLineLengthFound {
+			lineLength++
+		}
+		if char == '\n' {
+			isLineLengthFound = true
+			countLine++
+		}
+	}
+	fmt.Println("longueur de la lignes :", lineLength)
+	fmt.Println("Nombre de lignes:", countLine)
+}
