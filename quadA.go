bc871447472b34558c6d984eede10247ffc25d50
KevinCastel <keviniicastel@gmail.com>
2023-05-06 21:33:43 +0200

    quadA.go

--- quadA.go
+++ quadA.go
@@ -3,30 +3,29 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-<<<<<<< HEAD
-	quadA(0, 1)
-=======
-	quadA(1, 0)
->>>>>>> 809e685 (ajout des fichiers)
+	QuadA(12, 8)
 }
 
-func quadA(x, y int) {
-	for h := 1; h <= y; h++ {
-		for l := 1; l <= x; l++ {
-			if h == 1 || h == y {
-				if l == 1 || l == x {
+func QuadA(x, y int) {
+	if x <= 0 || y <= 0 {
+	} else {
+		for a := 1; a <= y; a++ {
+			for b := 1; b <= x; b++ {
+				if (a == 1 && b == 1) || (a == 1 && b == x) || (a == y && b == 1) || (a == y && b == x) {
 					z01.PrintRune('o')
-				} else {
-					z01.PrintRune('-')
-				}
-			} else {
-				if l == 1 || l == x {
+				} else if b == 1 && (a != y || a != 1) {
 					z01.PrintRune('|')
+				} else if b == x && (a != 1 || a != y) {
+					z01.PrintRune('|')
+				} else if a == 1 && (b != 1 || b != x) {
+					z01.PrintRune('-')
+				} else if a == y && (b != 1 || b != x) {
+					z01.PrintRune('-')
 				} else {
 					z01.PrintRune(' ')
 				}
 			}
+			z01.PrintRune('\n')
 		}
-		z01.PrintRune('\n')
 	}
 }
