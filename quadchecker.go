f6c153a21b7f34111d3029242351720d4bb4d359
Castel <keviniicastel@gmail.com>
2023-05-06 12:44:35 +0000

    Rajout des docstring

--- quadchecker.go
+++ quadchecker.go
@@ -17,22 +17,47 @@ func main() {
 }
 
 
-func GetArgs(a[]string) map[string]string{
-  mapQuadInformation := map[string]string{}
+func GetArgs(a[]string) map[string]int{
+  /*
+    Itere tous les arguments afin d'y
+    recuperer les noms de fichier et les
+    valeurs
+  */
+  mapQuadInformation := map[string]int{}
+  isLastWasInt := false
+  key := ""
   for _, i := range a{
+    if IsInt(i){
+      if isLastWasInt{
+        isLastWasInt = false
+      }
+      key += string(i)
+    } else {
+      isLastWasInt = true
+      
+    }
     fmt.Println(i)
-    
   }
   return mapQuadInformation
 }
 
 func IsInt(s string) bool{
-  var b bool
-  
-  return b
+  /*
+    Appelé pour vérifier si
+    la chaine de caractere
+    contient des nombres.
+    Si slle ne contient pas
+    de nombres, c'est considere
+    comme un nom de fichier
+  */
+  isInt := true
+  for _,c := range s{
+    if c < '0' || c > '9'{
+      isInt = false
+    }
+  }
+  return isInt
 }
-
-
 
 func quadchecker(){
   /*
